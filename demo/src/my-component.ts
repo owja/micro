71ed19953d188ddb4444a57d0eadf521d4fd0788
Hauke Broer <hbroer@users.noreply.github.com>
2023-05-15 23:18:31 +0200

    enhance demo to use createRefresh

--- demo/src/my-component.ts
+++ demo/src/my-component.ts
@@ -1,31 +1,43 @@
-import {Micro} from "@owja/micro";
+import {createRefresh, Micro} from "@owja/micro";
+import styles from "./my-component.css";
+
+type HttpBinGetResponse = {
+    args: Record<string, string>,
+    headers: Record<string, string>,
+    origin: string,
+    url: string,
+}
 
 class MyComponent extends Micro {
-    wrapper = Micro.createRef<HTMLDivElement>();
+    wrapper: HTMLDivElement;
+    removeMe = Micro.createRef<HTMLDivElement>();
 
     constructor() {
-        super(`
-        .wrapper {
-            color: red;
-            cursor: pointer;
-        }
-        .clicked {
-            color: green;
-        }
-    `);
+        super(styles);
 
-        Micro.create("div", this.root, {
+        this.wrapper = Micro.create("div", this.root, {
             class: "wrapper",
             listener: {
-                click: () => this.wrapper.current?.classList.toggle("clicked")
-            },
-            ref: this.wrapper,
-            content: [
-                Micro.create("span", {
-                    content: "Hello World"
-                })
-            ],
+                click: () => {
+                    this.wrapper.classList.toggle("clicked");
+                    this.removeMe.current?.remove();
+                }
+            }
         });
+
+        createRefresh<HttpBinGetResponse>("https://httpbin.org/get", 60, (r) => this.render(r), {
+            parameters: { hello: "hello args" },
+            headers: { Hello: "hello headers" },
+        });
+    }
+
+    render(r: HttpBinGetResponse) {
+        Micro.clear(this.wrapper);
+        Micro.create("div", this.wrapper, { content: "Remove Me! (click)", ref: this.removeMe });
+        Micro.create("div", this.wrapper, { content: "Your IP: " + r.origin });
+        Micro.create("div", this.wrapper, { content: "Origin: " + r.headers["Origin"] });
+        Micro.create("div", this.wrapper, { content: "Test Arg: " + r.args["hello"] });
+        Micro.create("div", this.wrapper, { content: "Test Header: " + r.headers["Hello"] });
     }
 }
 
