6ed4695f6a4e9859177f5e9da0d4a0bc6168000b
Hauke Broer <hbroer@users.noreply.github.com>
2023-05-21 18:42:02 +0200

    add inline documentation and added types for the native webcomponents callbacks

--- src/micro.ts
+++ src/micro.ts
@@ -7,18 +7,68 @@ function isContent(value: any): value is Content {
     return typeof value === "string" || Array.isArray(value) || value instanceof Element;
 }
 
-type Options<Element> = {
+interface Options<Element> {
+    /**
+     * Optional ID set on the new HTML Element, can be true or string
+     */
     id?: true | string;
+    /**
+     * Optional css classname(s)
+     */
     class?: string;
+    /**
+     * Optional properties set on the new HTML Element
+     */
     props?: PropertyFilter<Element>;
+    /**
+     * Optional target (HTML Element or a ShadowRoot) where the new HTML Element is added to
+     */
     target?: HTMLElement | ShadowRoot;
+    /**
+     * Optional attributes set on the new HTML Element via `setAttribute(key, value)`
+     */
     attr?: {[key: string]: string};
+    /**
+     * Optional event listener added via `addEventListener(type, callback)`
+     */
     listener?: {[K in keyof HTMLElementEventMap]?: (this: Element, ev: HTMLElementEventMap[K]) => any};
+    /**
+     * A ref object created with `Micro.createRef()` or a callback
+     */
     ref?: Reference<Element> | ((el: Element) => any);
-};
+}
 
 let CurrentId = 0;
 
+export interface Micro {
+    /**
+     * Invoked each time the custom element is appended into a document-connected element.
+     * This will happen each time the node is moved, and may happen before the element's
+     * contents have been fully parsed.
+     */
+    connectedCallback?(): void;
+
+    /**
+     * Invoked each time the custom element is disconnected from the document's DOM.
+     */
+    disconnectedCallback?(): void;
+
+    /**
+     * Invoked each time the custom element is moved to a new document.
+     */
+    adoptedCallback?(): void;
+
+    /**
+     * Invoked each time one of the custom element's attributes is added, removed, or changed.
+     * Which attributes to notice change for is specified in a static get observedAttributes method.
+     */
+    attributeChangedCallback?(name: string, oldValue?: string, newValue?: string): void;
+}
+
+/**
+ * Base Class for Custom Components
+ * also provides some helper
+ */
 export abstract class Micro extends HTMLElement {
     protected root: ShadowRoot;
 
@@ -28,13 +78,28 @@ export abstract class Micro extends HTMLElement {
         styles && Micro.create("style", {target: this.root}, styles);
     }
 
+    /**
+     * Create an HTML Element
+     * by tagName
+     */
     static create<K extends keyof HTMLElementTagNameMap>(tagName: K): HTMLElementTagNameMap[K];
+
+    /**
+     * Create an HTML Element
+     * by tagName with string content or child elements
+     */
     static create<K extends keyof HTMLElementTagNameMap>(tagName: K, content: Content): HTMLElementTagNameMap[K];
+
+    /**
+     * Create an HTML Element
+     * by tagName with options and optional with string content or child elements
+     */
     static create<K extends keyof HTMLElementTagNameMap>(
         tagName: K,
         options: ElementCreationOptions & Options<HTMLElementTagNameMap[K]>,
         content?: Content,
     ): HTMLElementTagNameMap[K];
+
     static create<K extends keyof HTMLElementTagNameMap>(
         tagName: K,
         options: (ElementCreationOptions & Options<HTMLElementTagNameMap[K]>) | Content = {},
@@ -45,7 +110,7 @@ export abstract class Micro extends HTMLElement {
             options = {};
         }
 
-        const el = document.createElement(tagName, {is: options?.is});
+        const el = document.createElement(tagName, {is: options.is});
 
         if (options.class) {
             el.classList.add(options.class);
@@ -65,24 +130,18 @@ export abstract class Micro extends HTMLElement {
             el.id = options.id === true ? Micro.generateId() : options.id;
         }
 
-        if (options.listener) {
-            for (const key in options.listener) {
-                // @ts-ignore could not find another solution
-                el.addEventListener(key, options.listener[key]);
-            }
+        for (const key in options.listener) {
+            // @ts-ignore could not find another solution
+            el.addEventListener(key, options.listener[key]);
         }
 
-        if (options.props) {
-            for (const key in options.props) {
-                // @ts-ignore and again, could not find another solution (for now)
-                el[key] = options.props[key];
-            }
+        for (const key in options.props) {
+            // @ts-ignore and again, could not find another solution (for now)
+            el[key] = options.props[key];
         }
 
-        if (options.attr) {
-            for (const key in options.attr) {
-                el.setAttribute(key, options.attr[key]);
-            }
+        for (const key in options.attr) {
+            el.setAttribute(key, options.attr[key]);
         }
 
         if (options.ref) {
@@ -93,6 +152,9 @@ export abstract class Micro extends HTMLElement {
         return el;
     }
 
+    /**
+     * Removes all children from a DOM Element or ShadowRoot
+     */
     static clear(el: Element | ShadowRoot | null, keepStyles = true) {
         if (!el) return;
 
@@ -105,10 +167,16 @@ export abstract class Micro extends HTMLElement {
         keep.length && el.append(...keep);
     }
 
+    /**
+     * Generates an incremental ID "gid<number>"
+     */
     static generateId(): string {
         return "gid" + CurrentId++;
     }
 
+    /**
+     * Creates a simple Ref object
+     */
     static createRef<R>(init?: R): Reference<R> {
         return {current: init};
     }
